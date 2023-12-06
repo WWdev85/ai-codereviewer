06939342a3046b8db5e81d3ae0789d370345483c
Wojciech Wąsek <wasekw@gmail.com>
2023-12-06 11:01:23 +0100

    change file path

--- dist/index.js
+++ dist/index.js
@@ -117,7 +117,7 @@ function createPrompt(file, chunk, prDetails) {
 - Always propose the code to resolve given issue found by you.
 
 Always use following ESlint rules:
-${(0, fs_1.readFileSync)('rules.txt', 'utf8')}
+${(0, fs_1.readFileSync)('./rules.txt', 'utf8')}
 
 Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
 
